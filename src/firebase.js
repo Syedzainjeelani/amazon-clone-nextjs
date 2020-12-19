f8be60eb6281d57cceef16934c1a306f0d6aeded
Syedzainjeelani <sayedzainjeelani@gmail.com>
2020-12-19 20:20:51 +0500

    Fix next env variables issues: add .env.local file

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,18 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+
+// console.log(`API Key from process:: ${process.env.NEXT_PUBLIC_APIKEY}`)
 
 const firebaseConfig = {
-    apiKey: `${process.env.FIREBASE_API_KEY}`,
+    apiKey: `${process.env.NEXT_PUBLIC_APIKEY}`,
     authDomain: `${process.env.AUTHDOMAIN}`,
     projectId: `${process.env.PROJECTID}`,
     storageBucket: `${process.env.STORAGEBUCKET}`,
     messagingSenderId: `${process.env.MESSAGINGSENDERID}`,
-    appId: `${process.env.APPID}`,
+    appId: `${process.env.NEXT_PUBLIC_APPID}`,
     measurementId: `${process.env.MEASUREMENTID}`,
 }
 
