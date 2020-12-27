20df97966b06489a51b6ef1c13e67dac00e70594
Syedzainjeelani <sayedzainjeelani@gmail.com>
2020-12-27 23:43:42 +0500

    Fetch products from firestore db to load with serverside props

---
+++ src/config/data/products.js
@@ -0,0 +1,104 @@
+export const products = [{
+    "id": 1000000,
+    "title": "The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback",
+    "price": 11.96,
+    "rating": 5,
+    "image": "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+},
+{
+    "id": 1000001,
+    "title": "Acer SB220Q bi 21.5 Inches Full HD (1920 x 1080) IPS Ultra-Thin Zero Frame Monitor (HDMI & VGA Port), Black",
+    "price": 93.99,
+    "rating": 4.7,
+    "image": "https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SL1500_.jpg"
+},
+{
+    "id": 1000002,
+    "title": "Acer Aspire 5 Slim Laptop, 15.6 inches Full HD IPS Display, AMD Ryzen 3 3200U, Vega 3 Graphics, 4GB DDR4, 128GB SSD, Backlit Keyboard, Windows 10 in S Mode, A515-43-R19L, Silver",
+    "price": 364.99,
+    "rating": 4.6,
+    "image": "https://images-na.ssl-images-amazon.com/images/I/71vvXGmdKWL._AC_SL1500_.jpg"
+},
+{
+    "id": 1000003,
+    "title": "Seagate Portable 2TB External Hard Drive Portable HDD – USB 3.0 for PC, Mac, PS4, & Xbox - 1-year Rescue Service (STGX2000400)",
+    "price": 62.99,
+    "rating": 4.7,
+    "image": "https://images-na.ssl-images-amazon.com/images/I/81tjLksKixL._AC_SL1500_.jpg"
+},
+{
+    "id": 1000004,
+    "title": "BOBS from Skechers Women's Keepsakes Ice Angel Slipper",
+    "price": 96.24,
+    "rating": 4.6,
+    "image": "https://images-na.ssl-images-amazon.com/images/I/71mNMBUfjVL._AC_UX575_.jpg"
+},
+{
+    "id": 1000005,
+    "title": "Garmin Forerunner 235, GPS Running Watch, Black/Gray",
+    "price": 149.99,
+    "rating": 4.6,
+    "image": "https://images-na.ssl-images-amazon.com/images/I/816cZQLT7HL._AC_SL1500_.jpg"
+},
+{
+    "id": 1000006,
+    "title": "Bedtime Originals Twinkle Toes Pink Elephant Plush, Hazel",
+    "price": 8.59,
+    "rating": 5,
+    "image": "https://images-na.ssl-images-amazon.com/images/I/81ZB62prn-L._AC_SL1500_.jpg"
+},
+{
+    "id": 1000007,
+    "title": "Graco Landmark 3 in 1 Car Seat, Infant to Toddler Car Seat, Wynton",
+    "price": 153.99,
+    "rating": 5,
+    "image": "https://images-na.ssl-images-amazon.com/images/I/81XDqmqBCML._SL1500_.jpg"
+},
+
+{
+    "id": 1000008,
+    "title": "Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl",
+    "price": 239.0,
+    "rating": 4,
+    "image": "https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+
+},
+
+{
+    "id": 1000009,
+    "title": "Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor",
+    "price": 199.99,
+    "rating": 3,
+    "image": "https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+
+},
+
+{
+    "id": 1000010,
+    "title": "Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric",
+    "price": 98.99,
+    "rating": 5,
+    "image": "https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+
+},
+
+{
+    "id": 1000011,
+    "title": "New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)",
+    "price": 598.99,
+    "rating": 4,
+    "image": "https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+
+},
+
+{
+    "id": 1000012,
+    "title": "Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440",
+    "price": 1094.98,
+    "rating": 4,
+    "image": "https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+
+}
+
+
+]
--- src/config/index.js
+++ src/config/index.js
@@ -4,3 +4,22 @@ export const CURRENCY = 'usd'
 export const MIN_AMOUNT = 10.0
 export const MAX_AMOUNT = 5000.0
 export const AMOUNT_STEP = 5.0
+import { db } from '../firebase'
+import { products } from './data/products'
+
+
+export default function storeInventory() {
+
+
+    // db.collection("inventory")
+    //     .doc("inventory_products")
+    //     .set({
+    //         "products": products
+    //     })
+    //     .then((res) => {
+    //         console.log("Firestore Added inventory: ")
+    //     })
+    //     .catch(function (error) {
+    //         console.error("Error Adding to inventory: ", error);
+    //     });
+}
